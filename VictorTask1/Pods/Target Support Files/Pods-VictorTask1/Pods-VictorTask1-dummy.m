10674f29d54500026766561d15b3c4f27c8f9bfc
Victor Mironchik <v_mironchik@ibmgroup.eu>
2024-10-26 02:38:45 +0300

    change project

---
+++ VictorTask1/Pods/Target Support Files/Pods-VictorTask1/Pods-VictorTask1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VictorTask1 : NSObject
+@end
+@implementation PodsDummy_Pods_VictorTask1
+@end
---
+++ VictorTask1/VictorTask1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VictorTask1
+//
+//  Created by Mironchik on 26.10.24.
+//
+
+import UIKit
+import Realm
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
