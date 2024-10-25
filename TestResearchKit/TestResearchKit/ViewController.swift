4ffdd3c6481651ed9c28f20f670e7ff63cd2950d
Victor Mironchik <v_mironchik@ibmgroup.eu>
2024-10-25 13:40:06 +0300

    create empty project

---
+++ TestResearchKit/TestResearchKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestResearchKit
+//
+//  Created by Mironchik on 25.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
