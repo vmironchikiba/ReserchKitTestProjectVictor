8c35409848ea45e8db27f6272e6d3b2cca9d1f9d
Victor Mironchik <v_mironchik@ibmgroup.eu>
2024-10-28 11:32:50 +0300

    Updates for DB

--- VictorTask1/VictorTask1/RealmDB/DBManager.m
+++ VictorTask1/VictorTask1/RealmDB/DBManager.m
@@ -8,6 +8,10 @@
 #import "DBManager.h"
 #import "VictorTask1-Swift.h"
 
+@interface DBManager()
+ @property (nonatomic, strong) SurveyModel *model;
+@end
+
 @implementation DBManager
 
 + (instancetype)sharedInstance {
@@ -35,18 +39,76 @@
 - (void)saveSurveyRecord:(NSString *)firstName lastName: (NSString *)lastName age: (NSInteger) age interest: (NSInteger) interest  {
     NSError *error = nil;
     [self.realm transactionWithBlock:^{
-        SurveyModel *model = [[SurveyModel alloc] init];
-        model.firstName = firstName;
-        model.lastName = lastName;
-        model.age = age;
-        model.interest = interest;
-        [self.realm addObject:(RLMObject *)model];
+        
+        if (self.model == nil) {
+            self.model = [[SurveyModel alloc] init];
+        }
+        
+        self.model.firstName = firstName;
+        self.model.lastName = lastName;
+        self.model.age = age;
+        self.model.interest = interest;
+        [self.realm addObject:(RLMObject *)self.model];
     } error:&error];
     
     if (error) {
         NSLog(@"Error saving SurveyModel to Realm: %@", error.localizedDescription);
     } else {
         NSLog(@"SurveyModel saved successfully.");
+    }
+}
+
+
+- (void)saveFirstName:(NSString *)firstName  {
+    if (self.model == nil) {
+        self.model = [[SurveyModel alloc] init];
+    }
+    self.model.firstName = firstName;
+}
+
+- (void)saveLastName:(NSString *)lastName  {
+    if (self.model == nil) {
+        self.model = [[SurveyModel alloc] init];
+    }
+    self.model.lastName = lastName;
+}
+
+- (void)saveAge:(NSInteger) age{
+    if (self.model == nil) {
+        self.model = [[SurveyModel alloc] init];
+    }
+    self.model.age = age;
+}
+
+- (void)saveEmail:(NSString *)email {
+    if (self.model == nil) {
+        self.model = [[SurveyModel alloc] init];
+    }
+    
+    self.model.email = email;
+}
+
+- (void)saveInterests:(NSInteger) interest  {
+    if (self.model == nil) {
+        self.model = [[SurveyModel alloc] init];
+    }
+    
+    self.model.interest = interest;
+}
+
+- (void)commitRecord {
+    NSError *error = nil;
+    if (self.model == nil) {
+        [self.realm transactionWithBlock:^{
+            [self.realm addObject:(RLMObject *)self.model];
+        } error:&error];
+        
+        if (error) {
+            NSLog(@"Error saving SurveyModel to Realm: %@", error.localizedDescription);
+        } else {
+            NSLog(@"SurveyModel saved successfully.");
+            self.model = nil;
+        }
     }
 }
 
